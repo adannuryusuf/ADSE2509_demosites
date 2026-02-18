c26003ed6f3ae20aa3192ee3fb0a375cc1d86ec6
adannuryusuf <adannuryusuf1@gmail.com>
2026-02-18 12:05:07 +0300

    modifications

--- js/js_calculator.js
+++ js/js_calculator.js
@@ -1,39 +1,44 @@
-// Js file to demonstrate the various js operators and switch case 
-// construct to create a simple calc
-//create a functio n to hamdle te calc
+// JavaScript file to demonstrate the various JS operators and switch...case
+// construct to create a simple calculator.
+
+// Create a function to handle the calculation
 function calculate()
 {
-    //constants to hold the number entered by the user and the operation selexted
-    const num1 = parseInt(document.getElementById('txtFirstNum').value);
-    const num2 = parseInt(document.getElementById('txtSecondNum').value);
-    const operation = parseInt(document.getElementById('txtsltOperation').value);
-
-    let result = "";
-    switch(ooperation)
-    {
-        case "+":
-            result = num1 + num2;
-            break;
-        case "-":
-            result = num1 - num2;
-            break;
-        case "*":
-            result = num1 * num2;
-            break;
-        case "/"://handle when num2 is 0 for robustness
-            result = num1 / num2;
-            break;
-        case "%":
-            result = num1 % num2;
-            break;
-        case "^":
-            result = num1 ** num2; // same as math.pow
-            break;
-        default:
-            alert("Please select add ,subtract, multiply divide modulus power");
-            result = "";
-            break;
-    }
-    // display the result in the answer text field
-    document.getElementById('txtAnswer').value = result;
+	// Constants to hold the numbers/values entered by the user and the operation selected
+	const num1 = parseInt(document.getElementById('txtFirstNum').value);
+	const num2 = parseInt(document.getElementById('txtSecondNum').value);
+	const operation = document.getElementById('sltOperation').value;
+	
+	// Variable to store/hold the result/answer
+	let result = "";
+	
+	// Switch...case to determine the arithmetic operation to be performed
+	switch(operation)
+	{
+		case "+":
+			result = num1 + num2;
+			break;
+		case "-":
+			result = num1 - num2;
+			break;
+		case "*":
+			result = num1 * num2;
+			break;
+		case "/": // for robustness handle the case when num2 == 0 to avoid errors in your script
+			result = num1 / num2;
+			break;
+		case "%":
+			result = num1 % num2;
+			break;
+		case "^":
+			result = num1 ** num2; // Same as Math.pow(num1,num2)
+			break;
+		default:
+			alert("Please select Add, Subtract, Multiply, Divide, Modulus/Remainder of Exponent/Power")
+			result = "";
+			break;
+	}
+	
+	// Display the result/answer in the answer textfield (txtAnswer)
+	document.getElementById('txtAnswer').value = result;
 }
--- js/js_intro.js
+++ js/js_intro.js
@@ -1,15 +1,20 @@
-// JS document to demonstrate the if... else decision construct.
-// Variable to hold the id of the container 
+// JavaScript Document to demonstrate the if...else decision/selection construct.
+
+// Variable to hold the id of the container div
 let container = document.getElementById("container");
-// create a dib that will hold the contents of this JS document 
+
+// Create a div that will hold the contents of this JS document
 let selectionDiv = document.createElement('div');
-let age = 20, votingStatus = "";
-// Assign a voting status based on users age
-if (age>= 18)
-    votingStatus = `You are ${age} years old, hence can vote.`;
+let age = 10, votingStatus = "";
+
+// Assign a voting status based on the user's age
+if(age >= 18)
+	votingStatus = `You are ${age} years old, hence you can vote.`;
 else
-    votingStatus = `Unfortunetly you are ${age} and hence cannot vote.`;
-// Add the voting status message to the selectionDiv
+	votingStatus = `Unfortunately you are ${age} years old, hence you cannot vote.`;
+
+// Add the voting status message to the selectoinDiv
 selectionDiv.innerHTML = votingStatus;
-// Add the contents of the selection div to the container div
+
+// Add the contents of the selection Div to the container Div
 container.appendChild(selectionDiv);
